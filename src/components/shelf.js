7910b6496fecd63138bd9f52c084ef96267241e3
Prateek_Tewari <hindustani1000@gmail.com>
2018-11-12 22:22:58 +0530

    Components made

---
+++ src/components/shelf.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Book from "./book";
+
+class Shelf extends React.Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.name}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book, key) => (
+              <Book updateBook={this.props.updateBook} book={book} key={key} />
+            )) /*Shelf Update*/}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Shelf;
